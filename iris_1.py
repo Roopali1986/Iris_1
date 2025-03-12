0dfb6490c0605839b3b03a95c60921fe05d19c51
Roopali1986 <118526466+Roopali1986@users.noreply.github.com>
2025-03-12 23:09:11 +0530

    Update iris_1.py

--- iris_1.py
+++ iris_1.py
@@ -38,36 +38,7 @@ plt.savefig('decision_tree.png')
 # Print accuracy score
 print(f"Model training completed. Accuracy on training data: {dec_tree.score(inputs, target):.4f}")
 
-#### TASK:
-We have to build a decision tree that can effectively make suitable classifications, accurately, based on graphical visualizations.
 
-#### APPROACH:
-We could define the task assigned into two parts. Part one could comprise of a Decision Tree model that trains itself based on the provided data and make predictions while the second part could contain a more diverse function that enables us to plot the tree for any data. In this, we could define a function that takes the input of the input and target variables and directly plots the graph and makes classification based predictions. We can load iris as an in-built dataset but since we have access to the csv file in the local system directory, we will import the dataset from the local directory itself.
-"""
 
-# Commented out IPython magic to ensure Python compatibility.
-import pandas as pd
-from sklearn import tree
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder as le
-# %matplotlib inline
 
-iris = pd.read_csv('/content/Iris.csv')
-iris
-
-iris['Species'] = le.fit_transform(iris['Species'], iris['Species'])
-iris
-
-inputs = iris.drop(iris[['Id', 'Species']], axis =1)
-inputs
-
-target = iris[['Species']]
-target
-
-dec_tree = tree.DecisionTreeClassifier(random_state = 0)
-
-classifier = dec_tree.fit(inputs, target)
-
-plt.figure(figsize=(20,15))
-tree.plot_tree(classifier, filled = True)
 
